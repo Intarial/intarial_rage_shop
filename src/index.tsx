215d9a8b66577d6cff7c192a0b27087a81667edd
Nekogl <99093592+Intarial@users.noreply.github.com>
2022-11-29 13:01:03 +0300

    Initial

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Shop } from "./Shop";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Shop />
+  </React.StrictMode>
+);
